7cd62807dbde5bb441f3f91a8213e6cb7ea3d59e
Pinchuk <ek.pinchuk777@gmail.com>
2024-07-16 16:15:34 +0300

    Задание 2

--- js_lesson2.js
+++ js_lesson2.js
@@ -1,10 +1,18 @@
 'use strict'
 
-const product = 'Dress1';
+const product = 'Dress';
 const count = 5;
 const category = 'Clothes';
 const price = 2500;
-const totalAmount = count * price;
+
 
 console.log(product);
-console.log('The total amount of the product: ', totalAmount , ' R ');
+console.log('The total amount of the product: ${count * price} R ');
+
+const product1 = 'Mobile';
+const count1 = 8;
+const category1 = 'Tech';
+const price1 = 58000;
+
+console.log(product1);
+console.log('The total amount of the product: ${count1 * price1} R ')
