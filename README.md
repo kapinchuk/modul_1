d900e2fa840354355c5aed8843427ed4bfd4c7e5
Pinchuk <ek.pinchuk777@gmail.com>
2024-07-16 16:06:18 +0300

    Обновление

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # git-config
 
 git config --list
+git init
+git add . добавить все файлы
+git commit -m Сделать коммит
+git status 
+git diff
+git diff --color-words
---
+++ js_lesson2.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const product = 'Dress1';
+const count = 5;
+const category = 'Clothes';
+const price = 2500;
+const totalAmount = count * price;
+
+console.log(product);
+console.log('The total amount of the product: ', totalAmount , ' R ');
